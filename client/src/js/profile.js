fd3950029d6a458a90630f18115d228178d2a80b
namichaud <namichaud@users.noreply.github.com>
2020-11-07 19:32:52 -0500

    Can now update profile picture on profile webpage

--- client/src/js/profile.js
+++ client/src/js/profile.js
@@ -6,13 +6,12 @@ window.addEventListener('load', loadProfile());
 function loadProfile()
 {
     fetchProfile();
-
-
 }
 
 async function fetchProfile()
 {
-    const profileResponse = await fetch(url+'/user/profile', {
+    const profileResponse = await fetch(url+'/user/profile', 
+    {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -25,6 +24,36 @@ async function fetchProfile()
     const image = document.createElement('img');
     image.classList.add('profilepic');
     image.src = profile.profilePicture;
-    image.alt="profile picture";
+    image.alt='profile picture';
+    image.id='profpic';
     document.getElementById('profilepicture').appendChild(image);
+
+    document.getElementById('updateprofpicbutton').addEventListener('click' , () => updateProfPic(profile.id));
+
+}
+
+async function updateProfPic(id)
+{
+    const profileURL = document.getElementById('updateprofpic').value.toString();
+
+    console.log(id);
+    console.log(url);
+
+    const profPicResponse = await fetch(url+'/user/profilepicture/update', 
+    {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({userID: id, profilePicture: profileURL})
+    });
+
+    if(profPicResponse.ok)
+    {
+        document.getElementById('profpic').src = profileURL;
+    }
+    else
+    {
+        alert('Picture URL is not valid!');
+    }
 }
