d5b9830e1f6b2c583386cf102252fc893e2afa96
Timothy Shee <timothy_shee@hotmail.com>
2020-11-08 23:08:33 -0500

    Working post requests & bad status code error messages

--- client/src/js/register.js
+++ client/src/js/register.js
@@ -4,14 +4,26 @@ import {postData} from './utils.js'
 const url = 'http://localhost:8080';
 
 window.addEventListener("load", async function () {
+  // get input
+  const email = document.getElementById('email-form').value;
+  const username = document.getElementById('username-form').value;
+  const password = document.getElementById('password-form').value;
+  // send register request
   document.getElementById('register-btn').addEventListener('click', () => {
-    postData(`${url}/user/messages`, {'email':userID,'username':starCount,'password':gameID})
+    postData(`${url}/user/register`, {'email': email,'username': username,'password': password})
       .then(response => {
-        const status = response.status;
-        const data = response.json();
-        console.log(response.status);
-        console.log(response.json());
-        alert(data)
-      });
+        if (response.ok) {
+          return response.json()
+        } else if(response.status === 404) {
+          return Promise.reject('error 404')
+        } else {
+          return Promise.reject('some other error: ' + response.status)
+        }
+      })
+      .then(data => {
+        console.log(data);
+        alert(JSON.stringify(data));
+      })
+      .catch(error => console.log('error is', error));
   });
 });
--- client/src/js/signin.js
+++ client/src/js/signin.js
@@ -6,27 +6,34 @@ const url = 'http://localhost:8080';
 window.addEventListener("load", async function () {
   document.getElementById('signin-btn').addEventListener('click', () => {
     // get input
-    const username = document.getElementById('username-form');
-    const password = document.getElementById('password-form');
+    const username = document.getElementById('username-form').value;
+    const password = document.getElementById('password-form').value;
+    console.log(email)
+    // send login request
     postData(`${url}/user/login`, {'username': username,'password': password})
       .then(response => {
-        const status = response.status;
-        const data = response.json();
-        console.log(response.status);
-        console.log(response.json());
-        alert(data)
-        if (status === 200) {
-          // Check browser support
-          if (typeof(Storage) !== "undefined") {
-            const { userID } = data;
-            // Store
-            localStorage.setItem("username", username);
-            localStorage.setItem("userID", userID);
-          } else {
-            alert("Sorry, your browser does not support Web Storage...");
-          }
+        if (response.ok) {
+          return response.json()
+        } else if(response.status === 404) {
+          return Promise.reject('error 404')
+        } else {
+          return Promise.reject('some other error: ' + response.status)
         }
-      });
+      })
+      .then(data => {
+        // Check browser support
+        if (typeof(Storage) !== "undefined") {
+          const { userID } = data;
+          // Store
+          localStorage.setItem("username", username);
+          localStorage.setItem("userID", userID);
+        } else {
+          alert("Sorry, your browser does not support Web Storage...");
+        }
+        console.log(data);
+        alert(JSON.stringify(data));
+      })
+      .catch(error => console.log('error is', error));
   });
 });
 
