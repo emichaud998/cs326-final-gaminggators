db809930c418c70b8917e0209517fdc88b64e8d3
Timothy Shee <timothy_shee@hotmail.com>
2020-11-09 02:28:41 -0500

    Fixed to create card with JS components to have onclick function referenced correctly
    When it was an ES6 string, it turns into a String, so i can't make the onclick reference easily

--- client/src/js/messages.js
+++ client/src/js/messages.js
@@ -3,10 +3,13 @@ import {postData} from './utils.js'
 
 const url = 'http://localhost:8080';
 class MessagesList {
-  init (element) {
-    this.render(element)
+  constructor() {
+    this.messageList = []
   }
-  render (element) {
+  getMessageList() {
+    return this.messageList
+  }
+  init (element) {
     // set username and userID (not use, since endpoint only uses 1)
     let username = "", userID = undefined;
     if (typeof(Storage) !== "undefined") {
@@ -24,29 +27,96 @@ class MessagesList {
         }
       })
       .then(data => {
-        const messageList = data;
-        let fragment = document.createDocumentFragment()
-        for (let message of messageList) {
-          // messageCard - should be separate class.....but lazy
-          let messageWrapper = document.createElement('div');
-          // too lazy to figure out each JS element & every class name
-          messageWrapper.innerHTML = `<div class="card border-dark mb-3">
-              <div class="card-header"><i class="fa fa-user fa-lg"></i> ${message.sender}</div>
-              <div class="card-body text-dark">
-                <h5 class="card-title"><a href="#">${message.title}</a></h5>
-                <p class="card-text">${message.message}</p>
-                <button type="button" class="btn btn-danger float-sm-right"><i class="fa fa-trash"></i></button>
-              </div>
-            </div>`
-          // add to document fragment
-          fragment.appendChild(messageWrapper)
-        }
-        element.innerHTML = ""
-        element.appendChild(fragment);
+        this.messageList = data;
+        this.render(element);
       })
       .catch(error => console.log('error is', error));
   }
+  deleteItem(element, messageID) {
+    // set username and userID (not use, since endpoint only uses 1)
+    let username = "", userID = undefined;
+    if (typeof(Storage) !== "undefined") {
+      username = localStorage.getItem("username");
+      // userID = localStorage.getItem("username");
+    } else {
+      alert("Sorry, your browser does not support Web Storage...");
+    }
+    // makes post request to remove given message & rerenders
+    postData(`${url}/user/messages/remove`, {'username': username, 'userID': userID, 'messageID': messageID })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          return Promise.reject('HTTP STATUS CODE: ' + response.status)
+        }
+      })
+      .then(data => {
+        this.messageList = data;
+        this.render(element);
+      })
+      .catch(error => console.log('error is', error));
+  }
+  /* Card Example (for reference):
+    <div id=${message.id} class="card border-dark mb-3">
+      <div class="card-header"></div>
+      <div class="card-body text-dark">
+        <h5 class="card-title"><a href="#">${message.title}</a></h5>
+        <p class="card-text">${message.message}</p>
+        <button id="mybtn ${message.id}" type="button" class="btn btn-danger float-sm-right"><i class="fa fa-trash"></i></button>
+      </div>
+    </div>
+  */
+  render(element) {
+    let fragment = document.createDocumentFragment()
+    for (let message of this.messageList) {
+      // messageCard - should be separate class...
+      const messageWrapper = document.createElement('div');
+      const cardElem = document.createElement('div');
+      cardElem.classList.add("card", "border-dark", "mb-3");
+      // header
+      const cardHeaderElem = document.createElement('div');
+      cardHeaderElem.classList.add("card-header");
+      cardHeaderElem.innerHTML = `<i class="fa fa-user fa-lg"></i> ${message.sender}`
+      // body wrapper
+      const cardBodyElem = document.createElement('div');
+      cardBodyElem.classList.add("card-body", "text-dark");
+      // card body title
+      const cardBodyTitleElem = document.createElement('h5');
+      cardBodyTitleElem.classList.add("card-title");
+      cardBodyTitleElem.appendChild(document.createTextNode(`${message.title}`)); // add link here?
+      // card body message
+      const cardBodyMessageElem = document.createElement('p');
+      cardBodyMessageElem.classList.add("card-text");
+      cardBodyMessageElem.appendChild(document.createTextNode(`${message.message}`)); // add link here?
+      // card body button
+      const cardBodyButtonElem = document.createElement('button');
+      cardBodyButtonElem.classList.add("btn", "btn-danger", "float-sm-right");
+      cardBodyButtonElem.setAttribute("id", `mybtn ${message.id}`);
+      cardBodyButtonElem.innerHTML = `<i class="fa fa-trash"></i>`;
+      cardBodyButtonElem.onclick = () => {
+        this.deleteItem(element, message.id);
+      };
+      // add content to card body
+      cardBodyElem.appendChild(cardBodyTitleElem);
+      cardBodyElem.appendChild(cardBodyMessageElem);
+      cardBodyElem.appendChild(cardBodyButtonElem);
+      // add card header and body to card div 
+      cardElem.appendChild(cardHeaderElem);
+      cardElem.appendChild(cardBodyElem);
+      // add card div to wrapper
+      messageWrapper.appendChild(cardElem);
+      // add each card wrapper to document fragment
+      fragment.appendChild(messageWrapper)
+    }
+    element.innerHTML = ""
+    element.appendChild(fragment);
+  }
 }
 
-let messageList = new MessagesList()
-messageList.init(document.getElementById("messageList"))
+window.addEventListener("load", async function () {
+  const msgListElem = document.getElementById("messageList");
+  let messageListComp = new MessagesList();
+  messageListComp.init(msgListElem);
+});
+
+// CHANGE IT TO PROPER JS way (way too much of a pain)
