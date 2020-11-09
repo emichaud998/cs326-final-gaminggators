84e0c1fca4cce556660a8b0654736648266a4aa2
Timothy Shee <timothy_shee@hotmail.com>
2020-11-08 19:26:56 -0500

    Added signin input ids & load it from JS

--- client/src/js/signin.js
+++ client/src/js/signin.js
@@ -5,13 +5,33 @@ const url = 'http://localhost:8080';
 
 window.addEventListener("load", async function () {
   document.getElementById('signin-btn').addEventListener('click', () => {
-    postData(`${url}/user/login`, {'username': userID,'password': starCount})
+    // get input
+    const username = document.getElementById('username-form');
+    const password = document.getElementById('password-form');
+    postData(`${url}/user/login`, {'username': username,'password': password})
       .then(response => {
         const status = response.status;
         const data = response.json();
         console.log(response.status);
         console.log(response.json());
         alert(data)
+        if (status === 200) {
+          // Check browser support
+          if (typeof(Storage) !== "undefined") {
+            const { userID } = data;
+            // Store
+            localStorage.setItem("username", username);
+            localStorage.setItem("userID", userID);
+          } else {
+            alert("Sorry, your browser does not support Web Storage...");
+          }
+        }
       });
   });
 });
+
+function processAjaxData(response, urlPath){
+  document.getElementById("content").innerHTML = response.html;
+  document.title = response.pageTitle;
+  window.history.pushState({"html":response.html,"pageTitle":response.pageTitle},"", urlPath);
+}
