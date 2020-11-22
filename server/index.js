5c93deba9d30dfe97337f6b181735cc1d1dd47f8
Timothy Shee <timothy_shee@hotmail.com>
2020-11-22 04:35:57 -0500

    Change title based if isRatingList

--- server/index.js
+++ server/index.js
@@ -1040,12 +1040,13 @@ app.post('/messages/send', async (req, res) => {
     // const message = req.body['message'];
 
     if (req.user !== undefined) {
-        if (friendUsername !== undefined && gameIDList !== undefined) {
+        if (friendUsername !== undefined && gameIDList !== undefined && isRatingList !== undefined) {
             const username = (await query.execOne('username', 'users', 'id = $1', [req.user.id])).username;
-            const friendID = (await findUser(friendUsername)).id
+            const friendID = (await findUser(friendUsername)).id;
+            const isRatingList = "rating" in gameIDList[0];
             
             if (username && friendID) {
-                const title = `${username} Sent You Their Wishlist`
+                const title = isRatingList ? `${username} Sent You Their Rating List` : `${username} Sent You Their Wishlist` 
                 const nextMessageId = (await query.countRowsTable('user_messages')).count;
                 await query.insertInto('user_messages', '($1, $2, $3, $4)', [friendID, nextMessageId, title, JSON.stringify(gameIDList)]);
                 res.status(200).json({message: 'Successfully sent message to friend'});
